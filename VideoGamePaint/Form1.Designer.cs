229d3be12354a713e47eea9d650085f056753dbe
shieldgenerator7 <shieldgenerator7@yahoo.com>
2020-01-18 14:53:26 -0500

    Pixel grid drawing

--- VideoGamePaint/Form1.Designer.cs
+++ VideoGamePaint/Form1.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace VideoGamePaint
 {
-    partial class Form1
+    partial class frmPaint
     {
         /// <summary>
         /// Required designer variable.
@@ -28,13 +28,33 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.pnlPaint = new System.Windows.Forms.Panel();
+            this.SuspendLayout();
+            // 
+            // pnlPaint
+            // 
+            this.pnlPaint.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pnlPaint.Location = new System.Drawing.Point(0, 0);
+            this.pnlPaint.Name = "pnlPaint";
+            this.pnlPaint.Size = new System.Drawing.Size(800, 450);
+            this.pnlPaint.TabIndex = 0;
+            this.pnlPaint.Paint += new System.Windows.Forms.PaintEventHandler(this.pnlPaint_Paint);
+            // 
+            // frmPaint
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.Controls.Add(this.pnlPaint);
+            this.Name = "frmPaint";
+            this.Text = "Video Game Paint";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Panel pnlPaint;
     }
 }
 
--- VideoGamePaint/Form1.cs
+++ VideoGamePaint/Form1.cs
@@ -10,11 +10,71 @@ using System.Windows.Forms;
 
 namespace VideoGamePaint
 {
-    public partial class Form1 : Form
+    public partial class frmPaint : Form
     {
-        public Form1()
+        const int GRID_SIZE = 100;
+        const int PIXEL_SIZE = 8;
+        Color[,] pixelGrid;
+        Dictionary<Color, Brush> colorBrushes = new Dictionary<Color, Brush>();
+        //List<List<Color>> pixelGrid;
+
+        public frmPaint()
         {
             InitializeComponent();
+            pixelGrid = new Color[GRID_SIZE, GRID_SIZE];
+        }
+
+        private void pnlPaint_Paint(object sender, PaintEventArgs e)
+        {
+            Pen p = new Pen(Color.Red);
+            Brush b = new SolidBrush(Color.Red);
+            Graphics g = e.Graphics;
+            //g.DrawRectangle(p, 10, 10, 100, 100);
+            //g.FillRectangle(b, 10, 10, 100, 100);
+            for (int x = 0; x < GRID_SIZE; x++)
+            {
+                for (int y = 0; y < GRID_SIZE; y++)
+                {
+                    Random r = new Random(x * y);// *(int)System.DateTime.Now.Ticks);
+                    pixelGrid[x, y] = Color.FromArgb(
+                        r.Next() % 256,
+                        r.Next() % 256,
+                        r.Next() % 256
+                        );
+                }
+            }
+            for (int x = 0; x < GRID_SIZE; x++)
+            {
+                for (int y = 0; y < GRID_SIZE; y++)
+                {
+                    g.FillRectangle(
+                        getBrush(pixelGrid[x, y]),
+                        x * PIXEL_SIZE,
+                        y * PIXEL_SIZE,
+                        PIXEL_SIZE,
+                        PIXEL_SIZE
+                        );
+                }
+            }
+        }
+
+        Brush getBrush(Color color)
+        {
+            if (color == null)
+            {
+                color = Color.Red;
+            }
+            Brush brush;
+            if (colorBrushes.ContainsKey(color))
+            {
+                brush = colorBrushes[color];
+            }
+            else
+            {
+                brush = new SolidBrush(color);
+                colorBrushes.Add(color, brush);
+            }
+            return brush;
         }
     }
 }
