ad72027e97ba894c6f6887868647844185c6f9ee
shieldgenerator7 <shieldgenerator7@yahoo.com>
2020-01-19 14:41:18 -0500

    HandTool activates on mouse wheel down

--- VideoGamePaint/HandTool.cs
+++ VideoGamePaint/HandTool.cs
@@ -1,12 +1,28 @@
 ﻿using System;
+using System.Windows.Forms;
 using VideoGamePaint;
 
 public class HandTool : Tool
 {
     Vector firstMapPos = new Vector(0, 0);
+    Tool prevTool = null;
 
     public HandTool(PixelGridPanel pgp) : base(pgp)
     {
+        pixelGridPanel.MouseDown += overtakeMouseDown;
+    }
+
+    private void overtakeMouseDown(object sender, MouseEventArgs e)
+    {
+        if (e.Button == MouseButtons.Middle)
+        {
+            prevTool = pixelGridPanel.activeTool;
+            pixelGridPanel.activeTool = this;
+        }
+        else
+        {
+            prevTool = null;
+        }
     }
 
     public override void preactivate(int ex, int ey)
@@ -21,5 +37,15 @@ public class HandTool : Tool
         pixelGridPanel.mapPos.x = firstMapPos.x + (ex - pixelGridPanel.firstMousePosition.x);
         pixelGridPanel.mapPos.y = firstMapPos.y + (ey - pixelGridPanel.firstMousePosition.y);
         pixelGridPanel.Invalidate();
+    }
+
+    public override void postactivate(int ex, int ey)
+    {
+        base.postactivate(ex, ey);
+        if (prevTool)
+        {
+            pixelGridPanel.activeTool = prevTool;
+            prevTool = null;
+        }
     }
 }
--- VideoGamePaint/Tool.cs
+++ VideoGamePaint/Tool.cs
@@ -21,4 +21,9 @@ public abstract class Tool
     {
 
     }
+
+    public static implicit operator bool (Tool a)
+    {
+        return a != null;
+    }
 }
