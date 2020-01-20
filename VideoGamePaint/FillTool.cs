39dd4500ed09fe922ccf815749d41152a72352b5
shieldgenerator7 <shieldgenerator7@yahoo.com>
2020-01-19 21:26:07 -0500

    Fixed fill tool

--- VideoGamePaint/FillTool.cs
+++ VideoGamePaint/FillTool.cs
@@ -11,24 +11,48 @@ public class FillTool : Tool
     public override void activate(int ex, int ey)
     {
         PixelGrid pg = pixelGridPanel.pixelGrid;
-        RGB baseColor = pg.getPixel(
-            pixelGridPanel.gridPixelX(ex),
-            pixelGridPanel.gridPixelY(ey)
-            );
-        for (int x = 0; x < pg.Size.x; x++)
+        int gx = pixelGridPanel.gridPixelX(ex);
+        int gy = pixelGridPanel.gridPixelY(ey);
+        RGB baseColor = pg.getPixel(gx, gy);
+        RGB toColor = PixelGridPanel.ColorToRGB(pixelGridPanel.drawColor);
+        if (baseColor != toColor)
         {
-            for (int y = 0; y < pg.Size.y; y++)
-            {
-                if (pg.getPixel(x, y) == baseColor)
-                {
-                    pg.setPixel(
-                        x,
-                        y,
-                        PixelGridPanel.ColorToRGB(pixelGridPanel.drawColor)
-                        );
-                }
-            }
+            fillArea(gx, gy, baseColor, toColor);
+            pixelGridPanel.Invalidate();
         }
-        pixelGridPanel.Invalidate();
+    }
+
+    private void fillArea(int gx, int gy, RGB fromRGB, RGB toRGB)
+    {
+        //Exit if the pixel doesn't meet the requirements
+        if (!canFillPixel(gx, gy, fromRGB))
+        {
+            return;
+        }
+        //Set this pixel to the toRGB
+        pixelGridPanel.pixelGrid.setPixel(gx, gy, toRGB);
+        //Find the next pixels to set
+        fillArea(gx - 1, gy, fromRGB, toRGB);
+        fillArea(gx + 1, gy, fromRGB, toRGB);
+        fillArea(gx, gy - 1, fromRGB, toRGB);
+        fillArea(gx, gy + 1, fromRGB, toRGB);
+    }
+
+    protected virtual bool canFillPixel(int gx, int gy, RGB fromRGB)
+    {
+        //No if outside the grid
+        if (gx < 0 || gx >= pixelGridPanel.pixelGrid.Size.x
+            || gy < 0 || gy >= pixelGridPanel.pixelGrid.Size.y)
+        {
+            return false;
+        }
+        //No if no longer in the fromRGB area
+        RGB rgb = pixelGridPanel.pixelGrid.getPixel(gx, gy);
+        if (rgb != fromRGB)
+        {
+            return false;
+        }
+        //Else yes
+        return true;
     }
 }
